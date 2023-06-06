48a180f295e062b83eabda6744fcd1e7d939891e
Aashish Waghmare <waghmare.2502@gmail.com>
2023-06-06 17:49:05 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import openai
+import config
+
+app = Flask(__name__, static_url_path='/static')
+
+api_key = config.API_KEY
+openai.api_key = api_key
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    condition = request.form['condition']
+    severity = request.form['severity']
+
+    
+    res = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=f'Condition: {condition}, Severity: {severity}\nUser:',
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    reply = res.choices[0].text.strip()
+
+    return render_template('reply.html', condition=condition, severity=severity, reply=reply)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
