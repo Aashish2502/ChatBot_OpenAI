5752056e5a661183977c25fdea43de658ab8708b
Aashish Waghmare <waghmare.2502@gmail.com>
2023-06-06 18:27:29 +0530

    update port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import openai
 import config
+import os
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -32,4 +33,5 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
